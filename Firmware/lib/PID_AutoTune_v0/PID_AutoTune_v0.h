103a23ea52db20b58165fb6b2fc0b8170592a948
nichlock <nichlock@gmail.com>
2021-09-05 14:29:51 -0400

    Fixed a warning in the PID libraries

--- Firmware/lib/PID_AutoTune_v0/PID_AutoTune_v0.h
+++ Firmware/lib/PID_AutoTune_v0/PID_AutoTune_v0.h
@@ -1,5 +1,8 @@
 #ifndef PID_AutoTune_v0
 #define PID_AutoTune_v0
+
+// Fixes warning
+#undef LIBRARY_VERSION
 #define LIBRARY_VERSION	0.0.1
 
 class PID_ATune
@@ -10,24 +13,24 @@ class PID_ATune
   //commonly used functions **************************************************************************
     PID_ATune(double*, double*);                       	// * Constructor.  links the Autotune to a given PID
     int Runtime();						   			   	// * Similar to the PID Compue function, returns non 0 when done
-	void Cancel();									   	// * Stops the AutoTune	
-	
-	void SetOutputStep(double);						   	// * how far above and below the starting value will the output step?	
-	double GetOutputStep();							   	// 
-	
+	void Cancel();									   	// * Stops the AutoTune
+
+	void SetOutputStep(double);						   	// * how far above and below the starting value will the output step?
+	double GetOutputStep();							   	//
+
 	void SetControlType(int); 						   	// * Determies if the tuning parameters returned will be PI (D=0)
-	int GetControlType();							   	//   or PID.  (0=PI, 1=PID)			
-	
+	int GetControlType();							   	//   or PID.  (0=PI, 1=PID)
+
 	void SetLookbackSec(int);							// * how far back are we looking to identify peaks
 	int GetLookbackSec();								//
-	
+
 	void SetNoiseBand(double);							// * the autotune will ignore signal chatter smaller than this value
 	double GetNoiseBand();								//   this should be acurately set
-	
+
 	double GetKp();										// * once autotune is complete, these functions contain the
-	double GetKi();										//   computed tuning parameters.  
+	double GetKi();										//   computed tuning parameters.
 	double GetKd();										//
-	
+
   private:
     void FinishUp();
 	bool isMax, isMin;
@@ -37,7 +40,7 @@ class PID_ATune
 	int controlType;
 	bool running;
 	unsigned long peak1, peak2, lastTime;
-	int sampleTime;
+	unsigned int sampleTime;
 	int nLookBack;
 	int peakType;
 	double lastInputs[101];
@@ -49,7 +52,6 @@ class PID_ATune
 	double oStep;
 	double outputStart;
 	double Ku, Pu;
-	
+
 };
 #endif
-
