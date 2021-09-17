9c8c1e040b67b0b32a8422a1265db584d6446fc5
Nicholas Steele <nichlock@gmail.com>
2021-09-16 22:27:01 -0400

    Add G1 speed warning for bad speed inputs

--- Firmware/src/gcode/commands/g1.cpp
+++ Firmware/src/gcode/commands/g1.cpp
@@ -8,7 +8,10 @@ void GcodeExecuter::g1(GcodeCommand_t cmd){
   GcodeArg_t motor = gcodeParseValueFor('m', cmd);
   GcodeArg_t speed = gcodeParseValueFor('s', cmd);
   if(motor == GCODE_ARG_VALUE_ERR) motor = -1; // Set to invalid motor number
-  if(speed == GCODE_ARG_VALUE_ERR) speed = 0;
+  if(speed == GCODE_ARG_VALUE_ERR) {
+    LOG_WARN("Motor speed invalid; setting to zero\n");
+    speed = 0;
+    }
   if (motor_handler.isValidMotor(motor)) {
     LOG_INFO(F("Setting motor speed\n"));
     motor_handler.set_speed(motor, speed);
